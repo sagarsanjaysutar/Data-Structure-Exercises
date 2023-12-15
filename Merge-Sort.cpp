38c9cd83752e676524efd086b859b4ab2b30a87b
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2023-12-15 09:11:00 +0530

    Debugging Merge Sort

--- Merge-Sort.cpp
+++ Merge-Sort.cpp
@@ -3,8 +3,57 @@
 using namespace std;
 
 
+void printVec(vector<int> x){
+    for(int element :  x){
+        cout << element << " ";
+    }
+    cout << "\n";
+}
+
 vector<int> merge(vector<int> leftVec, vector<int> rightVec){
     vector<int> result;
+    
+
+    if(leftVec.size() == 1 && rightVec.size() == 1){
+        cout << "Both size 1\n";
+        if(leftVec.at(0) < rightVec.at(0)){
+            result.push_back(leftVec.at(0));
+            result.push_back(rightVec.at(0));
+        }
+        else{
+            result.push_back(rightVec.at(0));
+            result.push_back(leftVec.at(0));
+        }
+        return result;
+    }
+
+    int idx = 0;
+    while(leftVec.size() > 1){
+        cout << "Sorting left\n";
+        if(leftVec.at(idx) < leftVec.at(idx+1))
+            result.push_back(leftVec.at(idx));
+        else
+            result.push_back(leftVec.at(idx));
+
+        idx++;
+    }
+
+    int idx = 0;
+    while(rightVec.size() > 1){
+        cout << "Sorting Right\n";
+        if(rightVec.at(idx) < rightVec.at(idx+1))
+            result.push_back(rightVec.at(idx));
+        else
+            result.push_back(rightVec.at(idx));
+                    
+        idx++;
+    }
+    
+    cout << "Left Vec: ";
+    printVec(leftVec);
+    cout << "Right Vec: ";
+    printVec(rightVec);
+    
     result.reserve(leftVec.size() + rightVec.size());
     result.insert(result.end(), leftVec.begin(), leftVec.end());
     result.insert(result.end(), rightVec.begin(), rightVec.end());
@@ -12,7 +61,9 @@ vector<int> merge(vector<int> leftVec, vector<int> rightVec){
 }
 
 vector<int> mergeSort(vector<int> vec){
-
+    cout << "-- " << vec.size() << " | ";
+    printVec(vec);
+    
     if (vec.size() == 1) {
         return vec;
     }
@@ -22,20 +73,69 @@ vector<int> mergeSort(vector<int> vec){
         int end = vec.size() - 1;
         int mid = vec.size() / 2;
 
+        cout << "xxxxxx\n";
         vector<int> leftVec(mergeSort(vector<int>(vec.begin(), vec.begin() + mid)));
+        cout << "=====\n";
         vector<int> rightVec(mergeSort(vector<int>(vec.begin() + mid, vec.end())));
+        cout << "yyyyy\n";
 
         return merge(leftVec, rightVec);
     }
 }
 
+
 int main(){
     
     vector<int> x = {8, 12, 17, 13, 8};
-    for(int element :  mergeSort(x)){
+    cout << "pppppppppppppp ";
+    printVec(mergeSort(x));
+    cout << "\n\n";
+
+    return 0;
+}
+
+
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printVec(vector<int> x){
+    for(int element :  x){
         cout << element << " ";
     }
+    cout << "\n";
+}
 
-
+int main()
+{
+    vector<int> rightVec = {8, 12, 17, 13, 8};
+    vector<int> result;
+    
+    
+    int low = 0;
+    for(int i = 0, j = 0; j < rightVec.size()-1; j++){
+        
+        // if(rightVec.at(j) < rightVec.at(j+1))
+        //   result.push_back(rightVec.at(j));
+        // else
+        //   result.push_back(rightVec.at(j+1));
+        cout << i << " " << j << endl;
+        
+        if(j == rightVec.size() - 1){
+            i++;
+        }
+        
+    }
+    
+    printVec(result);
     return 0;
 }
