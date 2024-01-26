96cf275d9d3c5c5fee88a8978e30ef7e62ea8e4e
Sagar S Sutar <sutarsagarsanjay@gmail.com>
2024-01-26 05:54:36 +0530

    Finished merge sort

--- Quick-Sort.cpp
+++ Quick-Sort.cpp
@@ -1,37 +1,45 @@
 /**
- * \brief QuickSort: Divide & Conquer. Select a pivot point. 
- * On left would be values less than pivot & on right values greater than pivot.
+ * \brief QuickSort: Divide & Conquer. 
+ * 1. Select a pivot point. 
+ * 2. Rearrange vec such that smaller elements are on left of pivot & greater ones are on right.
+ * 3. Divide the rearranged vec. from the pivot point recursively.
+ * 4. Merge the result two vec. & return.
+ * 
+ * \note The rearrange part isn't the most optimal as it makes the solution O(n^2)
 */
 
 #include <iostream>
 #include <vector>
+#include <map>
 #include "Utils.cpp"
 
 using namespace std;
 
-int rearrangeVec(vector<int>& vec, int low, int high){
+map<int, vector<int>> rearrangeVec(vector<int> vec){
     
     // Right most element is the pivot.
-    int pivotIdx = high;
+    int pivotIdx = vec.size() - 1;
 
-    int idx = low;
+    int idx = 0;
 
-    // Place all the smaller elements to the left & move the greater element to the right of the pivot.
+    // Place all the smaller elements to the left of the pivot & greater ones to the right.
     while(idx < pivotIdx){
+
+        // Condition: If maxVal is found, push it to the end.
+        // Note: This is a non-optimal solution because of the 2 loops.
         if(vec.at(idx) > vec.at(pivotIdx)){
             int maxVal = vec.at(idx);
-            // cout << vectorToString(vec) << endl;
 
             // Place the maxVal at the end of vector & shift all elements backward by 1.
-            for(int i = idx; i < high; i++){
-                // cout << maxVal << "\t" << vectorToString(vec) << endl;
+            for(int i = idx; i < vec.size() - 1; i++){
                 vec.at(i) = vec.at(i + 1);
             }
-            vec.at(high) = maxVal;
+            vec.at(vec.size() - 1) = maxVal;
 
             // Since maxVal is moved to the end, pivot is shifted by -1
             pivotIdx--;
 
+            // Check if the mutated vector follows the Condition
             if(vec.at(idx) > vec.at(pivotIdx)){
                 continue;
             }
@@ -40,30 +48,32 @@ int rearrangeVec(vector<int>& vec, int low, int high){
         idx++;
     }
     
-    // cout << "Final \t" << vectorToString(vec) << "\t"<< pivotIdx << endl;
-    // cout << pivotIdx << "\t" << low << "\t" << high << endl;
-    return pivotIdx;
-
+    return {{pivotIdx, vec}};
 }
 
-
-void quickSort(vector<int>& vec, int low, int high){
-    if(high < low){
-        return;
+vector<int> quickSort(vector<int> vec){
+    if(vec.size() <= 1){
+        return vec;
     }
-     
-    int pi = rearrangeVec(vec, low, high);
-    // recursive call on the left of pivot
     
-    quickSort(vec, low, pi - 1);
-    quickSort(vec, pi + 1, high);
+    // Rearrange the vector
+    map<int, vector<int>> res = rearrangeVec(vec);
+    int pivotIdx = res.begin()->first;
+    vector<int> rearrangedVec = res.begin()->second;
 
+    // Recursively divide the vector from the pivot point
+    vector<int> leftVec(quickSort(vector<int>(rearrangedVec.begin(), rearrangedVec.begin() + pivotIdx)));
+    vector<int> rightVec(quickSort(vector<int>(rearrangedVec.begin() + pivotIdx, rearrangedVec.end())));
+
+    // Merge the two sorted vector
+    leftVec.insert(leftVec.end(), rightVec.begin(), rightVec.end());
+
+    return leftVec;
 }
 
 int main(){
     vector<int> vec({8, 1, 2, 3, 2, 4, 5, 3, 6, 7, 1});
-    quickSort(vec, 0, vec.size() - 1);
-
-    cout << "xx" << vectorToString(vec) << endl;
+    cout << "Original Vector: " << vectorToString(vec) << endl;
+    cout << "Sorted Vector: " << vectorToString(quickSort(vec)) << endl;
     return 0;
 }
